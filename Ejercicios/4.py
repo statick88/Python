2602de3acee9592552efd5cd7e5b13f4ceb7b406
5t4t1ck <dsaavedra88@gmail.com>
2015-03-06 15:27:59 -0500

    Agregando ejercicio 4.py

---
+++ Ejercicios/4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2.7
+#!-*- coding:utf-8 -*-
+'''
+Ingresar 2 Numeros, sumarlos, restarlos, multiplicarlos y dividirlos
+'''
+n1 = int(input("Ingrese el primer número: "))
+
+n2 = int(input("Ingrese el segundo número: "))
+
+suma = n1 + n2
+
+print "%d + %d = %d"%(n1, n2, suma)
+
+resta = n1 - n2
+
+print "%d - %d = %d"%(n1, n2, resta)
+
+multiplicacion = n1 * n2
+
+print "%d * %d = %d"%(n1, n2, multiplicacion)
+
+divicion = n1 / n2
+
+print "%d / %d = %d"%(n1, n2, divicion)
