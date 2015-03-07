97e76140a740022a2c61716a3a68339357ee785a
5t4t1ck <dsaavedra88@gmail.com>
2015-03-06 23:53:01 -0500

    Creando series

---
+++ Series/calcular_multiplicacion.py
@@ -0,0 +1,6 @@
+n = 1
+h = 1
+while n <= 20:
+    h *=n
+    n+=1
+print h
---
+++ Series/serie_50_48_46.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2.7
+#!-*- coding:utf-8 -*-
+'''
+Calcular e imprimir la suma 50+48+46+44+…+20
+'''
+
+for i in range(50,20,-2):
+    print i
