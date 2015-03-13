9790f1ae2976c7afdd909451989a898d1d260334
5t4t1ck <dsaavedra88@gmail.com>
2015-03-13 10:45:17 -0500

    Corrigiendo errores

---
+++ Ejercicios/calcular_rangos.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2.7
+#!-*- coding:utf-8 -*-
+'''
+Introducir dos numeros por teclado.
+Imprimir los numeros que hay entre ellos comenzando por el mas pequeño.
+Contar cuantos hay y cuantos de ellos son pares. Calcular la suma de los pares
+'''
+
+class calcular:
+
+    def iniciar():
+
+        n1 = int(input("Ingrese el primer número: "))
+
+        n2 = int(input("Ingrese el segundo número: "))
+
+    def imprimir(n1,n2):
+
